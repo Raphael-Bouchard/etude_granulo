2b142283e579ce7bb6635a2cfb8bbc0ea4250c2d
Raphael-Bouchard <rbouchard@hotmail.fr>
2022-08-05 16:57:15 +0200

    j'ajoute les samples pour faire tourner le code penddnat les vanace ssur les sample deja créés

--- input_data.py
+++ input_data.py
@@ -5,11 +5,11 @@
 # grace a ce dictionnaire on va chosiir quelles simulations lancées
 dictionnaire_simu={
 # permet de crÉer les sample avec les informations données ci-avant
-'creation_sample' : False,
+'creation_sample' : True,
 # permet de savoir si l'on veut procéder à l'essaie trriaxial une fois que l'ensemble des sample aura ete créé
 # faire attention à ca, si on met un void ratio impossible à atteindre dans la liste,
 # le code de creation de sample tournera à l'infini
-'essaie_triaxial' : False,
+'essaie_triaxial' : True,
 # encore une fois permet de savoir si l'onva lancer le post_traitement en automatique par la suite
 'post_traitement' : True
 }
@@ -88,7 +88,7 @@ Input_StressMask          = 7
 #######################################################################################
 sigma1              = -100*10**3
 # vitesse de deplacement des murs dans certains cas : ici s'utilise dans CompressTriax_yade.py
-strainSpeed         = -0.01
+strainSpeed         = -10
 # objectif de compression triaxial
 Strain_Z_Goal       = 0.2
 
@@ -96,4 +96,4 @@ Strain_Z_Goal       = 0.2
 # une boucle dans le programme supprimera de la liste tous les void ratio inferrieu à 0.6
 # si cette boucle devient chiante et qu'il faut l'enelver cela ce passe dans ver;py à la
 # fonction GestionInputDonnes()
-ListTargetVoidRatio   = [0.8,0.7,0.615]
+ListTargetVoidRatio   = [0.8,0.7,0.62]
